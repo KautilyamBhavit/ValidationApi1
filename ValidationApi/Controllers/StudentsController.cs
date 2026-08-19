48e1a8e661670a4e6d023124d013bf66e272024a
BD <Avni@DESKTOP-T7VL0FD>
2026-08-19 14:35:42 +0530

    Add project files.

---
+++ ValidationApi/Controllers/StudentsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ValidationApi.Models;
+
+namespace ValidationApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult CreateStudent(Student student)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            return Ok(student);
+        }
+
+        [HttpGet("error")]
+        public IActionResult TestError()
+        {
+            throw new Exception("This is a test exception");
+        }
+    }
+}
---
+++ ValidationApi/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ValidationApi.Models
+{
+    public class Student
+    {
+        [Required(ErrorMessage = "Name Is Required.")]
+        [MaxLength(50, ErrorMessage = "Name cannot exceed 50 characters")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Email Is Required.")]
+        [EmailAddress(ErrorMessage = "Please enter a valid email address")]
+        public string? Email { get; set; }
+
+        [Range(18,60,ErrorMessage = "Age must be between 18 and 60")]
+        public int Age { get; set; }
+    }
+}
